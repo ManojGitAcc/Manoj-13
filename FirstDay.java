3df9b896f6d77f3d5c3ed1e3b679298b84ff6159
ManojGitAcc <manojkumar.gk13@gmail.com>
2022-02-17 11:49:57 +0530

    java

---
+++ FirstDay.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+public class FirstDay {
+	public void add() {
+int a=1;
+if (a<10) {
+	System.out.println("true");
+	
+}
+else {
+	System.out.println("false");}
+}
+	public static void main(String[] args) {
+		FirstDay day=new FirstDay();
+		day.add();
+	}
+}
+
---
+++ Forloop.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+public class Forloop {
+public void manoj() {
+	for (int i = 0; i < 5; i++) {
+		System.out.println(i);
+	}
+
+}
+public static void main(String[] args) {
+	Forloop ad=new Forloop();
+    ad.manoj();	
+}
+}
+  
